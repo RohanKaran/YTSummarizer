92a61fb8030f9a35ea59d498936c46747cfc2ade
Rohan Karan <rohankaran001@gmail.com>
2023-01-15 01:30:10 +0530

    ui improved

--- summarizer/app/core/redis.py
+++ summarizer/app/core/redis.py
@@ -2,4 +2,8 @@ import aioredis
 
 from app.core import config
 
-redis = aioredis.from_url("redis://" + config.REDIS_HOST, password=config.REDIS_PASSWORD, decode_responses=True)
+redis = aioredis.from_url(
+    "redis://" + config.REDIS_HOST,
+    password=config.REDIS_PASSWORD,
+    decode_responses=True,
+)
